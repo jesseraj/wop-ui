157b3db07aca0bf22569e5c243ce59598579041f
jesseraj <89629604+jesseraj@users.noreply.github.com>
2021-12-26 14:34:20 +0200

    asdasd

--- js/modify-meme.js
+++ js/modify-meme.js
@@ -1,5 +1,5 @@
 'use strict';
-const url = 'http://localhost:5000'; // change url when uploading to server
+const url = 'http://10.114.34.40:5000'; // change url when uploading to server
 
 // get query parameter
 const getQParam = (param) => {
