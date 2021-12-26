8d46c468898c693c150787d1a2820ce3c2b571a7
jesseraj <89629604+jesseraj@users.noreply.github.com>
2021-12-26 16:13:31 +0200

    asdasd

--- js/check-login.js
+++ js/check-login.js
@@ -1,6 +1,6 @@
 (async () => {
   'use strict';
-  const url = 'http://10.114.34.40:5000'; // change url when uploading to server
+  const url = 'http://localhost:5000'; // change url when uploading to server
 
   // check sessionStorage
   if (!sessionStorage.getItem('token') || !sessionStorage.getItem('user')) {
